c3cfe3d3090be49808cc714bea1ddf9aca9518db
kevinfair1 <kevinfairaso@gmail.com>
2020-11-12 17:44:51 -0500

    wk7feedback-kevin

--- try-catch.js
+++ try-catch.js
@@ -1,27 +1,28 @@
-
-
-
-try{
-    foo()
-} catch(err){
-    console.log(err.message);
-} finally{
-    console.log('This is always executed');
+try {
+  foo();
+} catch (err) {
+  console.log(err.message);
+} finally {
+  console.log("This is always executed");
 }
+
+//! Great job on steps 1-5 Audi!
 
 // 6
 
 function fizz() {
-    throw new Error("my error message");
+  throw new Error("my error message");
 }
 
-
-
+//! Perfect fizz() function.
 
 // 7
 
-try{
-    fizz()
-} catch{
-    console.log(Error.message);
+try {
+  fizz();
+} catch {
+  console.log(Error.message);
 }
+
+console.log(Error.message);
+//! Step 7 - You are missing the buzz() function here. The whole try/catch block should be enclosed within the buzz() function. You also need to pass (err) as a paramater on your catch block to execute properly. And lastly, Error.message
